77b828e2e80437e36c9893a2622729075d05c82f
Renato Ferreira <renatoferreira.dev@gmail.com>
2020-08-06 23:05:26 +0200

    Customize Schedule section

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,33 +1,119 @@
-import React, { FC } from 'react';
-import { FiPower } from 'react-icons/fi';
+import React, { FC, useState } from 'react';
+import { FiPower, FiClock } from 'react-icons/fi';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 import logoImg from '../../assets/logo.svg';
 
-const Dahsboard: FC = () => (
-  <Container>
-    <Header>
-      <HeaderContent>
-        <img src={logoImg} alt="logo" />
+const Dahsboard: FC = () => {
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="logo" />
 
-        <Profile>
-          <img
-            src="https://www.andersonsobelcosmetic.com/wp-content/uploads/2018/09/chin-implant-vs-fillers-best-for-improving-profile-bellevue-washington-chin-surgery.jpg"
-            alt="Beatriz"
-          />
+          <Profile>
+            <img
+              src="https://www.andersonsobelcosmetic.com/wp-content/uploads/2018/09/chin-implant-vs-fillers-best-for-improving-profile-bellevue-washington-chin-surgery.jpg"
+              alt="Beatriz"
+            />
 
-          <div>
-            <span>Welcome,</span>
-            <strong>Beatriz Gonçales</strong>
-          </div>
-        </Profile>
+            <div>
+              <span>Welcome,</span>
+              <strong>Beatriz Gonçales</strong>
+            </div>
+          </Profile>
 
-        <button type="button">
-          <FiPower />
-        </button>
-      </HeaderContent>
-    </Header>
-  </Container>
-);
+          <button type="button">
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Appointments</h1>
+          <p>
+            <span>Today</span>
+            <span>Day 06</span>
+            <span>Monday</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Next appointment</strong>
+            <div>
+              <img
+                src="https://www.andersonsobelcosmetic.com/wp-content/uploads/2018/09/chin-implant-vs-fillers-best-for-improving-profile-bellevue-washington-chin-surgery.jpg"
+                alt="Beatriz"
+              />
+
+              <strong>Beatriz Gonçales</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Morning</strong>
+
+            {[1, 2, 3].map(item => (
+              <Appointment key={item}>
+                <span>
+                  <FiClock />
+                  08:00
+                </span>
+
+                <div>
+                  <img
+                    src="https://www.andersonsobelcosmetic.com/wp-content/uploads/2018/09/chin-implant-vs-fillers-best-for-improving-profile-bellevue-washington-chin-surgery.jpg"
+                    alt="Beatriz"
+                  />
+
+                  <strong>Beatriz Gonçales</strong>
+                </div>
+              </Appointment>
+            ))}
+          </Section>
+
+          <Section>
+            <strong>Afternoon</strong>
+
+            {[1, 2].map(item => (
+              <Appointment key={item}>
+                <span>
+                  <FiClock />
+                  08:00
+                </span>
+
+                <div>
+                  <img
+                    src="https://www.andersonsobelcosmetic.com/wp-content/uploads/2018/09/chin-implant-vs-fillers-best-for-improving-profile-bellevue-washington-chin-surgery.jpg"
+                    alt="Beatriz"
+                  />
+
+                  <strong>Beatriz Gonçales</strong>
+                </div>
+              </Appointment>
+            ))}
+          </Section>
+        </Schedule>
+
+        <Calendar />
+      </Content>
+    </Container>
+  );
+};
 
 export default Dahsboard;
